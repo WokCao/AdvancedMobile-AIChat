26ac09b056845d1983c3d413fd99d1cb8311f7a3
WokCao <henryquoccao@gmail.com>
2025-03-06 15:37:20 +0700

    Create: login screen

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:ai_chat/widgets/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your AI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
