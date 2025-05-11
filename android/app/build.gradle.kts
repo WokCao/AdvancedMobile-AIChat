ddce34428a91fa59cb1e6b259a8f0b6ae798ac00
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2025-05-11 23:15:11 +0700

    prod configs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ai_chat"
+        applicationId = "com.jarvis.chatgem"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -31,24 +30,33 @@ android {
         versionName = flutter.versionName
     }
 
-    flavorDimensions "default"
+    flavorDimensions += "default"
     productFlavors {
-        dev {
-            dimension "default"
-            applicationIdSuffix ".dev"
-            resValue "string", "chat_gem", "ChatGEM Dev"
+        create("dev") {
+            dimension = "default"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+            resValue("string", "app_name", "ChatGEM Dev")
         }
-        prod {
-            dimension "default"
-            resValue "string", "chat_gem", "ChatGEM"
+        create("prod") {
+            dimension = "default"
+            resValue("string", "app_name", "ChatGEM")
+        }
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(project.property("RELEASE_STORE_FILE") as String)
+            storePassword = project.property("RELEASE_STORE_PASSWORD") as String
+            keyAlias = project.property("RELEASE_KEY_ALIAS") as String
+            keyPassword = project.property("RELEASE_KEY_PASSWORD") as String
         }
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
